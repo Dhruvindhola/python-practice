87ffe2012dc2102058d18bb071f98e4cfe0d247c
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-17 15:08:12 -0400

    second commit

--- Exercise for Beginners/Current_Number_and_Previous_number.py
+++ Exercise for Beginners/Current_Number_and_Previous_number.py
@@ -1,9 +1,11 @@
 print("Printing current and previous number and their sum is a range(15)")
 previous_num = 0
+
 # Loop from 1 to 20
 for i in range(1, 20):
     a_sum = previous_num + i
     print("Current Number", i, "Previous Number", previous_num, "Sum: ",a_sum)
+    
     # modify previous number 
     # set it to the current number
     previous_num = i
