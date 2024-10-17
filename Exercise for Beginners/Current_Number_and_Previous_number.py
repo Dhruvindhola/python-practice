c483b26c01ef8700e9742da9e4cfada640bb6d73
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-17 15:20:43 -0400

    second commit

--- Exercise for Beginners/Current_Number_and_Previous_number.py
+++ Exercise for Beginners/Current_Number_and_Previous_number.py
@@ -1,10 +1,11 @@
-# accept input string from a user
-word = input('Enter word ')
-print("Original String:", word)
+print("Printing current and previous number and their sum is a range(15)")
+previous_num = 0
 
-# using list slicing
-# convert string to list
-# pick only even index chars
-x = list(word)
-for i in x[0::2]:
-    print(i)
+# Loop from 1 to 20
+for i in range(1, 20):
+    a_sum = previous_num + i
+    print("Current Number", i, "Previous Number", previous_num, "Sum: ",a_sum)
+    
+    # modify previous number 
+    # set it to the current number
+    previous_num = i
