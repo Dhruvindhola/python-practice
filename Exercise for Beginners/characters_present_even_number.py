14aeb34ae3d4b68a587160847dd4fa10a4ab6d47
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-17 15:35:22 -0400

    first commit

--- Exercise for Beginners/characters_present_even_number.py
+++ Exercise for Beginners/characters_present_even_number.py
@@ -1,6 +1,10 @@
+# accept input string from a user
 word = input("Enter word and number:")
 print("Original String:", word)
 
+# using list slicing
+# convert string to list
+# pick only even index chars
 x=list(word)
 for i in x[0::3]:
     print(i)
