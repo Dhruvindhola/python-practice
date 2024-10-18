ff7b678814ac16f1103d5abff5811bd09c891fac
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-18 17:25:12 -0400

    first commit

---
+++ Ch5/Ch5_practice/Q4.py
@@ -0,0 +1,6 @@
+s = set()
+s.add(18)
+s.add(18.0) 
+s.add('18') # length of s after there operations?
+print(s)
+print(len(s))
--- Ch5/Set_Union_Intersection.py
+++ Ch5/Set_Union_Intersection.py
@@ -1,5 +1,11 @@
+# Define two sets
 set1 = {3, 65, 70}
-set2 = {1, 9, 8, 15}
+set2 = {1, 3, 8, 15}
 
+# Print the union of set1 and set2
+# The union combines all unique elements from both sets.
 print(set1.union(set2))
+
+# Print the intersection of set1 and set2
+# The intersection returns only the common elements in both sets.
 print(set1.intersection(set2))
