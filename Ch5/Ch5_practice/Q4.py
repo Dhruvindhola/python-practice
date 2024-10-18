57ef2aee91ff3483916cb5db56ea690de08bf30b
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-18 17:25:45 -0400

    first commit

--- Ch5/Ch5_practice/Q4.py
+++ Ch5/Ch5_practice/Q4.py
@@ -2,5 +2,7 @@ s = set()
 s.add(18)
 s.add(18.0) 
 s.add('18') # length of s after there operations?
+
 print(s)
+
 print(len(s))
--- Ch5/Ch5_practice/Q6.py
+++ Ch5/Ch5_practice/Q6.py
@@ -15,4 +15,9 @@ lang = input("Enter language name: ")
 
 d.update({name: lang})
 
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+d.update({name: lang})
+
 print(d)
