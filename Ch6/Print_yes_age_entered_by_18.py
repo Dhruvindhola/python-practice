033f8dc61285fb90ebc8f9cd9deb61e4dc41ec48
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-10-21 14:37:45 -0400

    first commit

---
+++ Ch6/Print_yes_age_entered_by_18.py
@@ -0,0 +1,10 @@
+# write a program to print yes when the age entered by the user is greater than or equal to 18.
+
+# Get input from the user
+age = int(input("Enter Your Age: "))
+
+# Check if age is greater than or equal to 18
+if age >= 18:
+    print("Yes")
+else:
+    print("No")
