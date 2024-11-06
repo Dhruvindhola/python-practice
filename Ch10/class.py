0fe740e22375d1a07434fe39837d598e083d0926
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-11-06 14:18:41 -0500

    first commit

---
+++ Ch10/class.py
@@ -0,0 +1,11 @@
+class Employee:
+    language = "python"
+    salary = 120000
+
+Ram = Employee()
+Ram.name = "Ram"
+print(Ram.name, Ram.language, Ram.salary)
+
+Om = Employee()
+Om.name = "Om"
+print(Om.name, Om.salary, Om.language)
---
+++ Ch10/instance_class_attribute.py
@@ -0,0 +1,7 @@
+class Employee:
+    language = "Python" # This is a class attribute
+    salary = 120000
+
+Ram = Employee()
+# Ram.language = "JavaScript" # This is an instance attribute
+print(Ram.language, Ram.salary)
