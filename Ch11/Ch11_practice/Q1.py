5c92b7f02b8a9e5d3e6b232281e1f6df8632bdcb
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-11-14 16:58:41 -0500

    first commit

---
+++ Ch11/Ch11_practice/Q1.py
@@ -0,0 +1,8 @@
+marks ={
+    "Dhruvin": 95,
+    "Amit": 56,
+    "Jeel": 71,
+    84: "harsh"
+}
+
+print(marks.get("Jeel"))
--- Ch5/dict_methods.py
+++ Ch5/dict_methods.py
@@ -5,14 +5,14 @@ marks ={
     84: "harsh"
 }
 
-# print(marks.items()) # This methods is items
-# print(marks.keys()) # This methods is keys
-# print(marks.values()) # This methods is values
+print(marks.items()) # This methods is items
+print(marks.keys()) # This methods is keys
+print(marks.values()) # This methods is values
 
-# marks.update({"Dhruvin": 91, "ayush": 68}) # This methods is Update
-# print(marks)
+marks.update({"Dhruvin": 91, "ayush": 68}) # This methods is Update
+print(marks)
 
-# print(marks.get("Jeel")) # prints output None
+print(marks.get("Jeel")) # prints output None
 # print(marks["Jeel1"]) # returns an error
 
 print(marks.pop("Amit")) # This methods is pop
