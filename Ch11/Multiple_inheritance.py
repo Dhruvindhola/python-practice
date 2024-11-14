cdb05dc71023f6182d42342c40d59a44480681e6
Dhruvindhola <dhruvindhola2121@gmail.com>
2024-11-13 20:51:34 -0500

    first commit

--- Ch11/Multiple_inheritance.py
+++ Ch11/Multiple_inheritance.py
@@ -4,12 +4,12 @@ class Employee:
     def show(self):
         print(f"The name of the Employee is {self.name} and the company is {self.company}")
 
-class coder:
+class Coder:
     language = "Python"
     def printLanguage(self):
         print(f"Out of all the language here is your language: {self.language}")
 
-class Programmer(Employee):
+class Programmer(Employee, Coder):
     company = "ITC Infotech"
     def showLanguage(self):
         print(f"The name is {self.company} and he is good with {self.language} language")
